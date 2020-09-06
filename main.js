af765a8493608b4eca903ce01a9c10f011fe6ace
Thao Ma <MaThao@gmail.com>
2020-09-05 18:55:35 -0600

    Iteration one done

---
+++ main.js
@@ -0,0 +1,45 @@
+var radioBtns = document.querySelectorAll('.radio-button')
+var letsCookBtn = document.querySelector('.cook-button')
+var clearBtn = document.querySelector('.clear-button')
+var resultPage = document.querySelector('.result-container')
+var cookPotImg = document.querySelector('.cooking-pot')
+
+letsCookBtn.addEventListener('click', getRandomDishes)
+
+function getRandomDishes() {
+  var dishOption = getSelectedInput();
+  var randomIndex = getRandomIndex(meals[dishOption]);
+  var randomDish = meals[dishOption][randomIndex];
+  resultPage.insertAdjacentHTML('beforeend', `<span class="selected-dish">${randomDish}</span>`)
+  showResult();
+};
+
+function getSelectedInput() {
+  var selected;
+  radioBtns.forEach(function(btn) {
+    if (btn.checked) {
+      selected = btn.value
+    }
+  })
+  return selected
+};
+
+function getRandomIndex(array) {
+  return Math.floor(Math.random() * array.length)
+};
+
+function showResult() {
+  cookPotImg.classList.add('hidden')
+  clearBtn.classList.remove('hidden')
+  resultPage.classList.remove('hidden')
+};
+
+function getEntireMeal() {
+  
+}
+
+//create a getEntireMeal function
+  //need to access our sides, entrees, desserts array
+  //need to randomize all three arrays together and create
+//insertAdjacentHTML onto the result page and hide pot image
+//make a new function for addEventListener that contains the function getRandomDishes and getEntireMeal
