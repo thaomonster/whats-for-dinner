ce931cc3afb5d8077d1405e1e481ce5e19cca62f
Thao Ma <MaThao@gmail.com>
2020-09-06 10:09:27 -0600

    Add multiple function for one event listener

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
-var radioBtns = document.querySelectorAll('.radio-button')
-var letsCookBtn = document.querySelector('.cook-button')
-var clearBtn = document.querySelector('.clear-button')
-var resultPage = document.querySelector('.result-container')
-var cookPotImg = document.querySelector('.cooking-pot')
+var radioBtns = document.querySelectorAll('.radio-button');
+var letsCookBtn = document.querySelector('.cook-button');
+var clearBtn = document.querySelector('.clear-button');
+var resultPage = document.querySelector('.result-container');
+var cookPotImg = document.querySelector('.cooking-pot');
 
-letsCookBtn.addEventListener('click', getRandomDishes)
+letsCookBtn.addEventListener('click', populateMeal);
 
-function getRandomDishes() {
-  var dishOption = getSelectedInput();
-  var randomIndex = getRandomIndex(meals[dishOption]);
-  var randomDish = meals[dishOption][randomIndex];
+function getRandomDishes(str) {
+  var randomIndex = getRandomIndex(meals[str]);
+  var randomDish = meals[str][randomIndex];
   resultPage.insertAdjacentHTML('beforeend', `<span class="selected-dish">${randomDish}</span>`)
   showResult();
 };
@@ -35,11 +34,22 @@ function showResult() {
 };
 
 function getEntireMeal() {
+  console.log('meal')
   
-}
+  showResult();
+};
 
+function populateMeal() {
+  var checked = getSelectedInput();
+  if (checked === 'meals') {
+    getEntireMeal();
+  } else {
+    getRandomDishes(checked)
+  }
+};
+
+//make a new function for addEventListener that contains the function getRandomDishes and getEntireMeal
 //create a getEntireMeal function
   //need to access our sides, entrees, desserts array
   //need to randomize all three arrays together and create
 //insertAdjacentHTML onto the result page and hide pot image
-//make a new function for addEventListener that contains the function getRandomDishes and getEntireMeal
